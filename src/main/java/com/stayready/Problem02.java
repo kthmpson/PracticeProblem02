ca2d77f0caa7bb82da526177be342a6afb665aaf
Kaiyen Thompson <kthompson6300@gmail.com>
2020-07-28 17:31:17 -0400

    done

--- src/main/java/com/stayready/Problem02.java
+++ src/main/java/com/stayready/Problem02.java
@@ -1,4 +1,25 @@
 package com.stayready;
 
+import java.util.Arrays;
+
 public class Problem02 {
+    public Integer continuousSequence(int[] numArray){
+        int loopCounter = 1;
+        int resultCounter = 0;
+        Arrays.sort(numArray);
+        for(int i = 1; i < numArray.length; i++){
+            if(numArray[i] - 1 == numArray[i-1]){
+                loopCounter++;
+            }
+            else{
+                loopCounter = 1;
+            }
+            if(resultCounter < loopCounter){
+                resultCounter = loopCounter;
+            }
+        }
+
+        return resultCounter;
+
+    }
 }
--- src/test/java/com/stayready/Problem02Test.java
+++ src/test/java/com/stayready/Problem02Test.java
@@ -1,4 +1,52 @@
 package com.stayready;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem02Test {
+
+    @Test
+    public void continuousSequence01(){
+        //Given
+        Problem02 problem02 = new Problem02();
+        int[] numArray = {10, 12, 11};
+        int expected = 3;
+
+        //When
+        int actual = problem02.continuousSequence(numArray);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void continuousSequence02(){
+        //Given
+        Problem02 problem02 = new Problem02();
+        int[] numArray = {14, 12, 11, 20};
+        int expected = 2;
+
+        //When
+        int actual = problem02.continuousSequence(numArray);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void continuousSequence03(){
+        //Given
+        Problem02 problem02 = new Problem02();
+        int[] numArray = {1, 56, 58, 57, 90, 92, 94, 93, 91, 45};
+        int expected = 5;
+
+        //When
+        int actual = problem02.continuousSequence(numArray);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+
+    }
 }
